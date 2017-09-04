0c2a6018e7ff1bfce31816625130cfc8262d3029
klfeiling <1243959772@qq.com>
2017-09-04 19:15:23 +0800

    修改了版权估值页面的输入搜索框

--- resources/views/PagesContents/Navigation1.blade.php
+++ resources/views/PagesContents/Navigation1.blade.php
@@ -4,13 +4,5 @@
             <h1>Copyright Valuation——电影版权估值平台 </h1>
         </a>
     </div>
-    <div class="search">
-        <div class="search2">
-            <form>
-                <i class="fa fa-search"></i>
-                <input type="text" value="搜索电影" onFocus="this.value = '';" onBlur="if (this.value == '') {this.value = '搜索电影';}"/>
-            </form>
-        </div>
-    </div>
     <div class="clearfix"></div>
 </div>
